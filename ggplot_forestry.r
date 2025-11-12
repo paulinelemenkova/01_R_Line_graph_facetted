f47dc7813d0423e73834cd1e4f9eba09a87b6c19
Polina Lemenkova <pauline.lemenkova@gmail.com>
2025-11-12 13:03:28 +0300

    Add files via upload

---
+++ ggplot_forestry.r
@@ -0,0 +1,49 @@
+# library
+library(ggplot2)
+library(tidyverse)
+library(readr)
+
+# Data
+df <- read_csv("Paper_production.csv")
+head(df)
+
+# ------------- facet_wrap ----------- >
+
+# Boxplots Grouping by 'category' using fill  for box plots and bar plots
+ggplot(df, aes(Item, Value, group = Item, fill = Item)) +
+    geom_boxplot() +
+    scale_color_brewer(palette = "Set1")
+
+options(scipen = 999)
+library(scales)
+library(ggplot2)
+# facetted plot
+p<- ggplot(df, aes(Year, Value, colour=Item)) +
+    geom_point(size=3, alpha = 0.5) +
+    geom_line() +
+#    geom_smooth(method = "loess") +
+    geom_smooth(method = "lm", linewidth = 0.5) + # Adds a linear regression line
+    facet_wrap(~Item, scales = "free", ncol = 3,
+        strip.position = "bottom") +
+    scale_color_brewer(palette = "Dark2") +
+    scale_y_continuous(
+    name = "Value (in 1000 tons)",
+        labels = function(y) y / 1000
+        ) +
+#    theme(legend.position = "bottom",
+#       legend.text = element_text(size = 10), ) +
+  theme(legend.position = c(1, 0),
+        legend.justification = c(1, 0),
+        legend.text = element_text(size = 10)) +
+    labs(color = "Forestry economics in Italy: Paper production \nin 1961-2023 (in 1000 tons). Data: FAO") +
+    guides(color = guide_legend(
+        nrow = 8,
+        theme = theme(
+            legend.title = element_text(size = 11, face = "plain",
+            colour = "black")
+    )))
+p
+# Source - https://stackoverflow.com/a/54438496
+ggsave("my_plot_1211.png", plot = p, dpi = 300, width = 12,
+    height = 10, units = "in")
+# or "top", "left", "right", "none"
