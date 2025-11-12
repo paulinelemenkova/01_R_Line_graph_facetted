abfd4a3f941f1b1e485f3afc41dc38aa102456a8
Polina Lemenkova <pauline.lemenkova@gmail.com>
2025-11-12 15:00:16 +0300

    Add files via upload

--- ggplot_20251211_export_quantity.r
+++ ggplot_20251211_export_quantity.r
@@ -2,16 +2,12 @@
 library(ggplot2)
 library(tidyverse)
 library(readr)
+library(viridis)
 
 df <- read_csv("Table_export_quantity.csv")
 head(df)
 
 # ------------- facet_wrap ----------- >
-
-# Boxplots Grouping by 'category' using fill  for box plots and bar plots
-ggplot(df, aes(Item, Value, group = Item, fill = Item)) +
-    geom_boxplot() +
-    scale_color_brewer(palette = "Set1")
 
 options(scipen = 999)
 library(scales)
@@ -25,7 +21,7 @@ p<- ggplot(df, aes(Year, Value, colour=Item)) +
     facet_wrap(~Item, scales = "free", ncol = 3,
         strip.position = "bottom") +
 #    scale_color_brewer(palette = "Dark2") +
-    scale_colour_viridis_d(option = "inferno") +
+    scale_colour_viridis_d(option = "cividis") +
     scale_y_continuous(
     name = "Value (in 1000 tons)",
         labels = function(y) y / 1000
@@ -35,15 +31,15 @@ p<- ggplot(df, aes(Year, Value, colour=Item)) +
   theme(legend.position = c(1, 0),
         legend.justification = c(1, 0),
         legend.text = element_text(size = 10)) +
-    labs(color = "Forestry economics in Italy: Export quantity (in 1000 tons). Data: FAO") +
+    labs(color = "Forestry economics in Italy: Export quantity \n(in 1000 tons). Data: FAO") +
     guides(color = guide_legend(
-        nrow = 5,
+        nrow = 8,
         theme = theme(
             legend.title = element_text(size = 11, face = "plain",
             colour = "black")
     )))
 p
 # Source - https://stackoverflow.com/a/54438496
-ggsave("my_plot.png", plot = p, dpi = 300,
-    width = 10, height = 8, units = "in")
+ggsave("my_plot_export.jpg", plot = p, dpi = 300,
+    width = 30, height = 25, units = "cm")
 # or "top", "left", "right", "none"
